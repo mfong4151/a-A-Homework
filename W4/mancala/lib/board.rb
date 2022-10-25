9312ec607c8b3ab779dad7507a2b83a3f620ee0d
mfong415 <85459923+mfong415@users.noreply.github.com>
2022-10-24 19:57:42 -0700

    Updated homework

--- W4/mancala/lib/board.rb
+++ W4/mancala/lib/board.rb
@@ -7,7 +7,8 @@ class Board
       @cups.each.with_index do |cup, i|
         next if i == 6 || i == 13
         @cups[i] = place_stones(cup)
-
+      @name1 = name1
+      @name2 = name2
       end
   end
 
@@ -35,16 +36,40 @@ class Board
 
   def make_move(start_pos, current_player_name)
  
-    distribute = @cups[start_pos].length
-    (start_pos +1...start_pos + distribute).each do |i|
-      @cups[i] << :stone
+    distribute = [] 
+    distribute.push(*@cups[start_pos])
+    @cups[start_pos] = []
 
+    start = start_pos + 1
+    
+    while distribute != []
+      if start == 13 
+        start = 0
+        next
+      end
+      @cups[start] << distribute.pop
+
+      start += 1
     end
-
+    next_turn(start)
+    render
   end
+
+
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 13 || ending_cup_idx == 6
+      return :prompt
+    
+   
+
+    elsif @cups[ending_cup_idx].count == 1
+    return :switch
+
+    else
+      return ending_cup_idx
+    end      
   end
 
   def render
@@ -56,8 +81,16 @@ class Board
   end
 
   def one_side_empty?
+    
+    side_1 = @cups[0...6]
+    side_2 = @cups[7...13]
+    return true if side_1.all?{|i| i.length == 0} || side_2.all?{|i| i.length == 0}
+    false
   end
 
   def winner
+    return :draw if @cups[6].length == @cups[13].length
+    return @name1 if @cups[6].length > @cups[13].length
+    return @name2 if @cups[6].length < @cups[13].length
   end
 end
