834475edf3f3f43a480a367c1a0bf4d3e082d033
mfong415 <85459923+mfong415@users.noreply.github.com>
2022-10-25 20:14:28 -0700

    Updated homework

--- W4/simon/lib/simon.rb
+++ W4/simon/lib/simon.rb
@@ -4,19 +4,30 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
 
   end
 
   def play
-
+    take_turn
   end
 
   def take_turn
-
+    until game_over
+     yellow show_sequence
+      round_success_message
+      require_sequence
+      self.sequence_length+= 1
+    end
+    game_over_message
+    reset_game
   end
 
   def show_sequence
-
+    add_random_color
+    self.seq
   end
 
   def require_sequence
@@ -24,10 +35,10 @@ class Simon
   end
 
   def add_random_color
-
+    self.seq << COLORS.sample()
   end
 
-  def round_success_message
+  def round_success_message()
 
   end
 
@@ -36,6 +47,8 @@ class Simon
   end
 
   def reset_game
-
+    self.seq = []
+    self.sequence_length = 1
+    self.game_over = false
   end
 end
