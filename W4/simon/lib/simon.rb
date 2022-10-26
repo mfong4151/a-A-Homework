ee3a1eb651f200288d2ef08db85bd922e11a9530
mfong415 <85459923+mfong415@users.noreply.github.com>
2022-10-25 20:34:15 -0700

    Updated homework

--- W4/simon/lib/simon.rb
+++ W4/simon/lib/simon.rb
@@ -15,23 +15,36 @@ class Simon
   end
 
   def take_turn
-    until game_over
-     yellow show_sequence
+
+    show_sequence
+    require_sequence
+
+    unless @game_over
       round_success_message
-      require_sequence
       self.sequence_length+= 1
     end
-    game_over_message
-    reset_game
   end
 
   def show_sequence
     add_random_color
-    self.seq
+    @seq.each do |color|
+      puts color
+      sleep 0.75
+      system("clear")
+      sleep 0.25
+    end
   end
 
   def require_sequence
-
+    puts "Repeat the sequence by entering the first letter of each color on a new line."
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
   end
 
   def add_random_color
